844ae0570aa3cc42263aabdeb963dd8f03114777
iraneyan <iraneyan18@gmail.com>
2026-03-01 17:27:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+AOS.init({
+  duration: 1000
+});
+
+var typed = new Typed("#typing", {
+  strings: [
+    "Full Stack Developer",
+    "DATA Analyst",
+    "Artificial Intelligence",
+    "cloud systems"
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
